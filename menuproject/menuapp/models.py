cf5e5e958f2cbaf06095f5c4a457a91358eb6373
androCyber <tatlicak@gmail.com>
2022-12-29 05:42:39 +0300

    Foreingkey exercises

--- menuproject/menuapp/models.py
+++ menuproject/menuapp/models.py
@@ -2,12 +2,15 @@ from unicodedata import name
 from django.db import models
 
 # Create your models here.
+class MenuCategory(models.Model):
+    menu_category_name=models.CharField(max_length=200)
 
 class Menu(models.Model):
     
     ad=models.CharField(max_length=100)
     mutfak=models.CharField(max_length=100)
     fiyat=models.IntegerField()
+    kategori_id=models.ForeignKey(MenuCategory,on_delete=models.PROTECT, default=None)
     
     def __str__(self):
         return self.name + " : "+ self.cuisine
