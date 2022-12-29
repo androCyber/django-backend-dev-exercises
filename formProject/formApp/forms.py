300977780d1fc139ba6fe32de6e185b2f3590acd
androCyber <tatlicak@gmail.com>
2022-12-29 06:20:14 +0300

    required change to false

--- formProject/formApp/forms.py
+++ formProject/formApp/forms.py
@@ -3,7 +3,7 @@ from django import forms
 SHIFTS=(("1","Morning"),("2","Afternoon"),("3","Evening"),)
 
 class InputForm(forms.Form):
-    firstName=forms.CharField(max_length=200)
+    firstName=forms.CharField(max_length=200,required=False)
     lastName=forms.CharField(max_length=200)
     shitf=forms.ChoiceField(choices=SHIFTS)
     time_log=forms.TimeField()
